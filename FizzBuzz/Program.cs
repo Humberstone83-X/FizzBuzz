faee3488c91a3a239d7ea0c8d76fda0511b547d1
Jack Humberstone <Jack.Humberstone@monek.com>
2025-09-19 11:26:46 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Enumerable.Range(1, 100).ToList();
+
+            foreach (int i in input)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
